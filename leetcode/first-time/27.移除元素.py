21702504d96bb510f39ef200082b284eff33aa1a
riza <yswisfree@gmail.com>
2023-08-14 22:19:04 +0800

    add: no.27

---
+++ leetcode/first-time/27.移除元素.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=27 lang=python
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution(object):
+    def removeElement(self, nums, val): 
+        slow = 0
+        fast = 0
+        while fast < len(nums):
+            if nums[fast]!=val :
+                nums[slow]=nums[fast]
+                slow +=1
+            fast += 1
+        return slow
+        
+# @lc code=end
+
